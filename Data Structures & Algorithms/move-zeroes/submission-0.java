1955d3b8875ddfb48148146df885041ae3bcbc75
Santosh Seelaboina <163726808+ssantoshhhhh@users.noreply.github.com>
2026-08-17 13:03:11 +0530

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp = nums[insertPos];
+                nums[insertPos] = nums[i];
+                nums[i]=temp;
+
+                insertPos++;
+            }
+        }
+    }
+}
